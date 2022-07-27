33ec3ac3a9e721baf8b9a5af3bb9ca6616c3533e
cogjava2205 <cogjava2205@ctsjava884.iiht.tech>
2022-07-27 09:47:06 +0530

    first commit

---
+++ Digital-Book_Notification/src/main/java/com/cog/DigitalBookNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.cog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBookNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBookNotificationApplication.class, args);
+	}
+
+}
