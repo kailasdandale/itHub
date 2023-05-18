db97c037b0a5f872097b44cd9e1a632cd3bdf970
kailasdandale <109525074+kailasdandale@users.noreply.github.com>
2023-05-18 10:45:02 +0530

    Update SpringBootSequirity3Application.java

--- src/main/java/com/cog/SpringBootSequirity3Application.java
+++ src/main/java/com/cog/SpringBootSequirity3Application.java
@@ -10,7 +10,7 @@ public class SpringBootSequirity3Application {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSequirity3Application.class, args);
 
-        System.out.println("Application Start !!!! OO");
+        System.out.println("Application Start !!!! O0OO");
     }
 
 
